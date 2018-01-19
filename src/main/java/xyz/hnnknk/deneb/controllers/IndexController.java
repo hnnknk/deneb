2f3ab830d416cbf2787cfdcd662e83a093b266f7
hnnknk <nevermoodjo@gmail.com>
2018-01-19 10:33:59 +0700

    Переделал конфигурацию spring mvc, перевел на аннотации.

--- src/main/java/xyz/hnnknk/deneb/controllers/IndexController.java
+++ src/main/java/xyz/hnnknk/deneb/controllers/IndexController.java
@@ -1,4 +1,4 @@
-package xyz.deneb.controllers;
+package xyz.hnnknk.deneb.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
