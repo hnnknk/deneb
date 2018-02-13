6ae07ae45844354dda7a20b7d026eec27d37bd6c
hnnknk <nevermoodjo@gmail.com>
2018-02-13 12:47:43 +0700

    Замена с3p0 на hikariCP

--- src/main/java/xyz/hnnknk/deneb/config/HibernateConfig.java
+++ src/main/java/xyz/hnnknk/deneb/config/HibernateConfig.java
@@ -27,9 +27,9 @@ public class HibernateConfig {
         sessionFactory.setDataSource(dataSource);
         sessionFactory.setPackagesToScan(new String[] {"xyz.hnnknk.deneb.model"});
         sessionFactory.setHibernateProperties(additionalProperties());
-
         return sessionFactory;
     }
+
 
     @Bean
     public DataSource dataSource(){
@@ -48,9 +48,10 @@ public class HibernateConfig {
         properties.setProperty("hibernate.show_sql", "true");
         properties.setProperty("hibernate.hbm2ddl.auto", "create-drop");
 
-        properties.setProperty("hibernate.c3p0.min_size", "5");
-        properties.setProperty("hibernate.c3p0.max_size", "20");
-        properties.setProperty("hibernate.c3p0.max_statements", "150");
+        properties.setProperty("hibernate.hikari.connectionTimeout", "20000");
+        properties.setProperty("hibernate.hikari.minimumIdle", "10");
+        properties.setProperty("hibernate.hikari.maximumPoolSize", "20");
+        properties.setProperty("hibernate.hikari.idleTimeout", "300000");
 
         return properties;
     }
