5777850fac21728c5914cd258cda7ed857c48611
hnnknk <nevermoodjo@gmail.com>
2018-03-19 15:13:00 +0700

    Теперь ангуляр создает путь к файлу html через функцию.

--- src/main/webapp/static/js/app.js
+++ src/main/webapp/static/js/app.js
@@ -9,68 +9,20 @@ App.config(['$httpProvider', function($httpProvider) {
 
 App.config(function($routeProvider){
     $routeProvider
-        .when('/monitor',{
-            templateUrl: 'static/views/monitor.html'
-
-        })
-        .when('/ups',{
-
-            templateUrl: 'static/views/ups.html'
-
-        })
-        .when('/mouse',{
-
-            templateUrl: 'static/views/mouse.html'
-
-        })
-        .when('/keyboard',{
-
-            templateUrl: 'static/views/keyboard.html'
-
-        })
-        .when('/notification',{
-
-            templateUrl: 'static/views/notification.html'
-
-        })
-        .when('/hdd',{
-
-            templateUrl: 'static/views/hdd.html'
-
-        })
-        .when('/ram',{
-
-            templateUrl: 'static/views/ram.html'
-
-        })
-        .when('/motherboard',{
-
-            templateUrl: 'static/views/motherboard.html'
-
-        })
-        .when('/powersupply',{
-
-            templateUrl: 'static/views/powersupply.html'
-
-        })
-        .when('/processor',{
-
-            templateUrl: 'static/views/processor.html'
-
-        })
         .when('/ro:param',{
-
+            
             templateUrl: 'static/views/ro.html'
 
         })
-        .when('/login',{
+        .when('/:path',{
 
-            templateUrl: 'static/views/login.html'
+            templateUrl: function(params){ return 'static/views/' + params.path + '.html'; }
 
         })
         .otherwise(
 
             { redirectTo: '/'}
+
         );
 
 });
